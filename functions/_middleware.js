001b1d755dc1ebea715fa033b41f4b96ba7faa93
u-tv <vikkythakur1995@gmail.com>
2026-06-08 16:17:34 +0530

    Delete functions/_middleware.js

--- functions/_middleware.js
+++
@@ -1,6 +0,0 @@
-// functions/_middleware.js
-export async function onRequest(context) {
-  // Allow dynamic routes to take precedence over static files
-  const response = await context.next();
-  return response;
-}
